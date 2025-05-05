9372d43b195bf496d73973fb0e66794eb10c4b34
Alex <ahoward2333@gmail.com>
2025-05-05 09:58:01 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+  return (
+    <button 
+      className="square" 
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>  
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>    
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>      
+    </>    
+  )
+}
